4440335a87c7f792e3f48d20d6aa761c33f6dc33
jasvirdosanjh <jasvirdosanjh@hotmail.com>
2017-04-11 19:16:20 -0700

    Update MenuBar.java
    name added (jasvir)

--- src/view/MenuBar.java
+++ src/view/MenuBar.java
@@ -52,7 +52,7 @@ public class MenuBar extends JMenuBar {
 		JMenuItem about = new JMenuItem("About...");
 		
 		about.addActionListener(new ActionListener() {
-			String names = "Matthew Phillips\nVidal Sisneros\n\n";
+			String names = "Matthew Phillips\nVidal Sisneros\nJasvir Dosanjh\n";
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				JOptionPane.showMessageDialog(myGUI, names, "About", JOptionPane.OK_OPTION, new ImageIcon());
